3cc2a54a8d39fd0a492322a5e8b479211e19bdfd
David Tang <tang.ddt@gmail.com>
2025-01-21 10:55:08 -0800

    getModel returns promise shape

--- cosmos/src/v2/get-model.ts
+++ cosmos/src/v2/get-model.ts
@@ -1,6 +1,27 @@
 import type { Spec, State } from "./core";
-import { initSpace } from "./cosmos";
+import { getPromise, initSpace } from "./cosmos";
+import type { Ready } from "./later";
 
-export function getModel<T>(spec: Spec<T>): State<T> {
-  return initSpace(spec).state;
+export type GetModelResult<T> = State<T> & PromiseShape<State<Ready<T>>>;
+
+type PromiseShape<T> = {
+  then: Promise<T>["then"];
+  catch: Promise<T>["catch"];
+  finally: Promise<T>["finally"];
+};
+
+export function getModel<T>(spec: Spec<T>): GetModelResult<T> {
+  const space = initSpace(spec);
+  return {
+    ...space.state,
+    get then() {
+      return getPromise(spec).then;
+    },
+    get catch() {
+      return getPromise(spec).catch;
+    },
+    get finally() {
+      return getPromise(spec).finally;
+    },
+  };
 }
